af877147cf7eee8385239bf99cade624e0ffc8ba
TissotPA <pierreantoine.tissot@teleflow.fr>
2026-03-16 17:00:22 +0100

    Commit - push automatique sur dev et sur gh-pages

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,9 @@
 # Publie le fichier coach_planner.json sur la branche gh-pages
 $date = Get-Date -Format "yyyy/MM/dd-HH:mm:ss"
 
+git add coach_planner.json
+git commit -m "Mise a jour donnees $date"
+git push
 git checkout gh-pages --force
 Remove-Item -Recurse -Force static, templates, __pycache__ -ErrorAction SilentlyContinue
 git checkout dev -- coach_planner.json
