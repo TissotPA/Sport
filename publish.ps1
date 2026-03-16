1f48dea03896f1644e590d298521bb81b36341d6
TissotPA <pierreantoine.tissot@teleflow.fr>
2026-03-16 16:31:28 +0100

    Add publish script for deploying coach_planner.json to gh-pages

---
+++ publish.ps1
@@ -0,0 +1,10 @@
+# Publie le fichier coach_planner.json sur la branche gh-pages
+$date = Get-Date -Format "yyyy/MM/dd-HH:mm:ss"
+
+git checkout gh-pages --force
+Remove-Item -Recurse -Force static, templates, __pycache__ -ErrorAction SilentlyContinue
+git checkout dev -- coach_planner.json
+git add coach_planner.json
+git commit -m "Mise a jour donnees $date"
+git push
+git checkout dev
